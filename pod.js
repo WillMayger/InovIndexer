d397509239afb144e0c07f8eef4990e14dd9517b
Will Mayger <will.mayger@theweblancer.com>
2017-08-25 13:39:33 +0100

    getting list of lessons and sub lessons!

--- pod.js
+++ pod.js
@@ -15,19 +15,19 @@ export class Pod {
   login() {
     return new Promise((resolve, reject) => {
       this.horseman
-        .open('https://www.frenchpod101.com/member/login_new.php')
-        .type('form[name="login"] input[name="amember_login"]', 'wmayger@outlook.com')
-        .type('form[name="login"] input[name="amember_pass"]', cred.toString())
-        .click('form[name="login"] input[name="remember_login"]')
-        .click('form[name="login"] button[type="submit"]')
-        .waitForNextPage()
-        .text('.page-redirect__title.ill-sakai')
-        .then((success) => {
-          if (success.toString().toLowerCase().indexOf('successfully')) {
-            resolve(true);
-          }
-          reject(false);
-        });
+      .open(this.baseURL + 'member/login_new.php')
+      .type('form[name="login"] input[name="amember_login"]', 'wmayger@outlook.com')
+      .type('form[name="login"] input[name="amember_pass"]', cred.toString())
+      .click('form[name="login"] input[name="remember_login"]')
+      .click('form[name="login"] button[type="submit"]')
+      .waitForNextPage()
+      .text('.page-redirect__title.ill-sakai')
+      .then((success) => {
+        if (success.toString().toLowerCase().indexOf('successfully')) {
+          resolve(true);
+        }
+        reject(false);
+      });
     });
   }
 
@@ -38,45 +38,40 @@ export class Pod {
   checkLogin() {
     return new Promise((resolve, reject) => {
       this.horseman
-        .open(this.baseURL)
-        .text('.dashbar-a__nav-item--sub.ill-ease-color')
-        .then((loggedInElem) => {
-          if (loggedInElem.toString().toLowerCase().indexOf('sign out')) {
-            resolve(true);
-          }
-          reject(false);
-        });
+      .open(this.baseURL)
+      .text('.dashbar-a__nav-item--sub.ill-ease-color')
+      .then((loggedInElem) => {
+        if (loggedInElem.toString().toLowerCase().indexOf('sign out')) {
+          resolve(true);
+        }
+        reject(false);
+      });
     });
   }
 
   getLessons() {
     return new Promise((resolve, reject) => {
       this.horseman
-        .open('https://www.frenchpod101.com/index.php?cat=Introduction')
-        .html()
-        .then((html) => {
-          let htmlDoc = cheerio.load(html);
-          htmlDoc('.ill-levels').map(function(i, elem) {
-            let levelObj = {};
-            let level = cheerio(elem).find('.ill-level-title');
-            let levelName = level.text();
-            if(levelName === 'News & Announcements') {
-              return true;
-            }
+      .open(this.baseURL + 'index.php?cat=Introduction')
+      .html()
+      .then((html) => {
+        cheerio(html).find('.ill-levels').map((i, elem) => {
+          let levelObj = {},
+              level = cheerio(elem).find('.ill-level-title'),
+              levelName = level.text();
+          if(levelName === 'News & Announcements') {
+            return true;
+          }
 
-            levelObj = {name: levelName, url: 'https:' + cheerio(elem).find('.ill-level-title').attr('href'), childlevels: []};
+          levelObj = {name: levelName, url: 'https:' + level.attr('href'), childlevels: []};
+          cheerio(elem).find('.ill-season-title').map((i, childelem) => {
+            levelObj.childlevels.push({name: cheerio(childelem).text(), url: cheerio(childelem).attr('href')})
+          });
+          this.lessons.levels.push(levelObj);
 
-            cheerio(elem).find('.ill-season-title').map(function(i, childelem) {
-              levelObj.childlevels.push({name: cheerio(childelem).text(), url: cheerio(childelem).attr('href')})
-            }.bind(this));
-
-            this.lessons.levels.push(levelObj);
-
-
-          }.bind(this));
-          resolve(this.lessons.levels);
         });
-        });
+        resolve(this.lessons.levels);
+      });
+    });
   }
-
 }
