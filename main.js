5ec6a5d8e86a2d4063b64e498c92cedb56df0e69
Will Mayger <will.mayger@theweblancer.com>
2017-09-03 21:44:47 +0100

    bugs ironed out

--- main.js
+++ main.js
@@ -4,34 +4,7 @@ const fs = require('fs');
 import { Pod } from './lib/pod';
 
 //usecase if starting from new.
-let pod = new Pod();
-
-pod.login()
-.then((result) => {
-  if (result) {
-    console.log('logged in successfully')
-  } else {
-    console.log('Not logged in...');
-    pod.end();
-    return 'err: not logged in';
-  }
-  return pod.getParentLessons();
-})
-.then(() => {return pod.getChildLessons()})
-.then(() => {return pod.getDownloadLinks()})
-.then(() => {return pod.write()})
-.catch((err) => {
-  console.log(err);
-  pod.end();
-});
-
-
-//usecase if already have json file indexing all lessons, sub lessons & download links
-//(generated & saved as lessons.json when other usecase has been run)
-//------------------------------------------------------------------------------------
-
-// var obj = JSON.parse(fs.readFileSync('lessons.json', 'utf8'));
-// let pod = new Pod(obj);
+// let pod = new Pod();
 //
 // pod.login()
 // .then((result) => {
@@ -42,12 +15,39 @@ pod.login()
 //     pod.end();
 //     return 'err: not logged in';
 //   }
-//   return pod.write();
+//   return pod.getParentLessons();
 // })
-// .then((res) => {
-//   console.log(res);
-//   return pod.end();})
+// .then(() => {return pod.getChildLessons()})
+// .then(() => {return pod.getDownloadLinks()})
+// .then(() => {return pod.write()})
 // .catch((err) => {
 //   console.log(err);
 //   pod.end();
 // });
+
+
+//usecase if already have json file indexing all lessons, sub lessons & download links
+//(generated & saved as lessons.json when other usecase has been run)
+//------------------------------------------------------------------------------------
+
+var obj = JSON.parse(fs.readFileSync('lessons.json', 'utf8'));
+let pod = new Pod(obj);
+
+pod.login()
+.then((result) => {
+  if (result) {
+    console.log('logged in successfully')
+  } else {
+    console.log('Not logged in...');
+    pod.end();
+    return 'err: not logged in';
+  }
+  return pod.write();
+})
+.then((res) => {
+  console.log(res);
+  return pod.end();})
+.catch((err) => {
+  console.log(err);
+  pod.end();
+});
