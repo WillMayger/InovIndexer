a626735540b3aa3bdca41a52c7ac0e8a6d9c8b7a
Will Mayger <will.mayger@theweblancer.com>
2017-09-05 11:22:57 +0100

    adding to readme

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# README #
+# Innovative Indexer #
+## Contents
+### 0. Intro.
+### 1. Usage - Starting Fresh (Most Cases).
+### 2. Usage - Starting with JSON File.
+### 3. Usage - Continuing with download.
 
-## todo
-https://www.npmjs.com/package/es6-promise-pool
+## 0. Intro
+  Indexes links and download links from certain websites and then downloads them.
+  Most of the application uses promises for ease.
+#### - Getting started
+1. clone this repository & make sure Node.js is installed (LTS) on your machine.
+2. run `npm install`.
+3. choose how you want to start the code (three choices below) all located in main.js.
+
+## 1. Usage - Starting Fresh
+ When running this for the first time you will want to use this option.
+
+## 2. Usage - Starting with JSON File.
+ When you have already run the first option once, if you ran it for enough time
+ it would have generated a `lessons.json` file within the folder `lessons/`.
+ You can then use this file to save time as this file contains all the indexing information.
+ Just plug this file into the initiation of the class and run the write function.
+
+## 3. Usage - Continuing with download.
+ Almost exactly the same as the previous step (2) however we need to pass in another
+ option which is `resume` and this needs to be set to `true`.
+ This will then continue downloading where you left off instead of starting again.
--- main.js
+++ main.js
@@ -31,35 +31,8 @@ import { Indexer } from './lib/indexer';
 //(generated & saved as lessons.json when other usecase has been run)
 //------------------------------------------------------------------------------------
 
-var obj = JSON.parse(fs.readFileSync('lessons.json', 'utf8'));
-let index = new Indexer({lessons: obj, resume: false});
-
-index.login()
-.then((result) => {
-  if (result) {
-    console.log('logged in successfully')
-  } else {
-    console.log('Not logged in...');
-    index.end();
-    return 'err: not logged in';
-  }
-  return index.write();
-})
-.then((res) => {
-  console.log(res);
-  return index.end();})
-.catch((err) => {
-  console.log(err);
-  index.end();
-});
-
-
-//usecase if you have already started writing the downloads and had to stop for one reason or another
-//(generated & saved as lessons.json when other usecase has been run)
-//------------------------------------------------------------------------------------
-
 // var obj = JSON.parse(fs.readFileSync('lessons.json', 'utf8'));
-// let index = new Indexer({lessons: obj, resume: true});
+// let index = new Indexer({lessons: obj, resume: false});
 //
 // index.login()
 // .then((result) => {
@@ -79,3 +52,29 @@ index.login()
 //   console.log(err);
 //   index.end();
 // });
+
+
+//usecase if you have already started writing the downloads and had to stop for one reason or another
+//(generated & saved as lessons.json when other usecase has been run)
+//------------------------------------------------------------------------------------
+
+var obj = JSON.parse(fs.readFileSync('lessons.json', 'utf8'));
+let index = new Indexer({lessons: obj, resume: true});
+
+index.login()
+.then((result) => {
+  if (result) {
+    console.log('logged in successfully')
+  } else {
+    console.log('Not logged in...');
+    index.end();
+    return 'err: not logged in';
+  }
+  return index.write();
+})
+.then(() => {
+  return index.end();})
+.catch((err) => {
+  console.log(err);
+  index.end();
+});
